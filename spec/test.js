38eaf7593f46e3be2cac650366b3c13e39583a39
ybinstock <ybinstock@gmail.com>
2014-10-10 14:37:51 -0700

    first version

--- spec/test.js
+++ spec/test.js
@@ -4,53 +4,75 @@ var expect = require("chai").expect;
 // all of these tests are pending...make sure to add , function(){ expect ....}
 
 describe('first', function() {
-  it('should be able to pull out the first element of an array');
-    // expect(myFunctions.first([1,2,3])).to.equal(1);
+  it('should be able to pull out the first element of an array', function () {
+    expect(myFunctions.first([1,2,3])).to.equal(1);
+    });
 
 
-  it('should be able to accept a user-defined index first([1,2,3],2) should return [1,2]');
+  it('should be able to accept a user-defined index first([1,2,3],2) should return [1,2]', function () {
+   expect(myFunctions.first([1,2,3],2)).to.eql([1,2]);
 
   });
+});
 
 
 describe('last', function() {
-  it('should pull the last element from an array');
+  it('should pull the last element from an array', function () {
+  expect(myFunctions.last([3,2,1])).to.equal(1);
+});
 
-  it('should accept an index argument');
+  it('should accept an index argument', function () {
+  expect(myFunctions.last([1,2,3],2)).to.eql([1,2]);
+  });
 
-  it('should return nothing if zero is passed in as the index');
+  it('should return nothing if zero is passed in as the index', function () {
+  expect(myFunctions.last([],0)).to.equal(null);
+  });
 
-  it('should return all the array\'s elements if the index argument is larger than the length of the array');
+  it('should return all the array\'s elements if the index argument is larger than the length of the array', function () {
+  expect(myFunctions.last([1,2,3,4],6)).to.eql([1,2,3,4]);
+  });
 
 });
 
 describe('uniq', function() {
-  it('should return all unique values contained in an unsorted array');
-
+  it('should return all unique values contained in an unsorted array', function () {
+  expect(myFunctions.uniq([1,1,2,3,4,4,4])).to.eql([1,2,3,4]);
+ });
 });
 
 
 describe('pluck', function() {
-  it('should return values contained at a user-defined property');
-    // var people = [
-    //   {name : 'moe', age : 30},
-    //   {name : 'curly', age : 50}
-    // ];
+  it('should return values contained at a user-defined property', function () {
 
-    // expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+var people = [
+      {name : 'moe', age : 30},
+      {name : 'curly', age : 50}
+    ];
+   expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+  });
+
 });
 
 describe('contains', function() {
-  it('should return false if a collection does not contain a user-specified value');
+  it('should return false if a collection does not contain a user-specified valueeeee', function () {
+    // console.log("array contains 6:", myFunctions.contains([3,1], 6));
+    expect(myFunctions.contains([3,1], 6)).to.equal(false);
+  });
 
-  it('should return true if a collection contains a user-specified value');
-    // expect(myFunctions.contains([  4,   5,   6], 5)).to.equal(true);
 
+  it('should return true if a collection contains a user-specified value', function () {
+    expect(myFunctions.contains([4, 5, 6], 5)).to.equal(true);
+  });
 });
 
 describe('flatten', function() {
-  it('can flatten nested arrays');
-    // var nestedArray = [1, [2], [3, [[[4]]]]];
+  it('can flatten nested arrays', function () {
+
+  });
+    var nestedArray = [1, [2], [3, [[[4]]]]];
 
     // expect(myFunctions.flatten(nestedArray)).to.eql([1,2,3,4]);
 });
+
+
--- src/underscore.js
+++ src/underscore.js
@@ -4,39 +4,95 @@ var myFunctions = {
   // return just the first element.
   first: function(array, n) {
 
+  // var taco = [];
+  if (typeof n === 'undefined') {
+    return array[0];
+  }
+  // if n is greater than the array length, return array
+  else if(array.length < n) {
+    return array;
+  }
+  else {
+   return array.splice(0,n);
+  }
+  //otherwise, return a new array starting from the beginning and going until
   },
+
+
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   last: function(array, n) {
+  // var taco = [];
+  if (typeof n==="undefined") {
+    return array[array.length-1];
+  }
+
+// should accept an index argument
+
+  else if (n===0) {return null;}
+
+  else if (array.length < n) {
+    return array;
+  }
+
+  else {
+    return array.splice(0,n);
+  }
+
 
   },
 
   // Produce a duplicate-free version of the array.
   uniq: function(array) {
+  var taco = [];
+  array.forEach(
+    function(elem)  {
+    if(taco.indexOf(elem) === -1) {
+      taco.push(elem);
+    }
 
+    });
+  return taco;
   },
+
 
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people objects
-  // (which have a name and an age) and return an array of just their ages
+  // (which have a name and an age) and return an array of just their names
   pluck: function(array, key) {
-
+    var powerCouple = [{name : "Yoni", age: 25}, {name:"Maia", age: 25}];
+    filterArray = array.map(function(item) {
+      return item[key];
+    });
+    return filterArray;
+    // return powerCouple;
   },
 
  // Determine if the array contains a given value (using `===`).
  // TIP: There are multiple solutions to this problem, ranging from one line to using reduce();
-  contains: function(array, target) {
+  contains: function(a, target) {
+    var result = false;
+    a.forEach( function(elem)  {
+      if (elem === target){
+        result = true;
+      }
+    });
+    return result;
+  }
+};
 
-  },
-
+// var sum = array.reduce()
   // Turns a multidimensional array into a single array
   // flatten([1,2,[3,4,[5,6]]]) returns [1,2,3,4,5,6]
   // Hint: Use Array.isArray to check if something is an array
   // Also, you will need to use recursion
   // when you find an array inside of an array
-  flatten: function(array) {
 
-  }
-};
+
+//   flatten: function(array) {
+//   var taco = [];
+
+//   }
+// };
 module.exports = myFunctions;
